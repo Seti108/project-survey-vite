e436e1bf756809259e277aa42269a84d65b501b2
SETI108 <seti108@gmail.com>
2023-10-25 11:44:24 +0200

    initial commit

---
+++ src/components/QuestionsAndAnswers/Question1.jsx
@@ -0,0 +1,10 @@
+export const Question1 = () => {
+  return (
+    <div>
+      <h1>What is your name?</h1>
+      <form action="">
+        <input type="text" title="Your name" placeholder="Your name" />
+      </form>
+    </div>
+  );
+};
